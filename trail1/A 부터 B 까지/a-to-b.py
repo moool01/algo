a9178c64a21fce603494021ae7cc29d7bc76ad86
moool01 <137693766+moool01@users.noreply.github.com>
2026-05-17 16:40:45 +0900

    [Passed] A 부터 B 까지 (45ms, 16MB)

--- trail1/A 부터 B 까지/a-to-b.py
+++ trail1/A 부터 B 까지/a-to-b.py
@@ -1,10 +1,8 @@
 a,b = map(int,input().split())
 
-print(a,end=" ")
-while a<b:
+while a<=b:
+    print(a,end=" ")
     if a%2 ==0:
         a +=3
     else:
         a *=2
-    if a<=b:
-        print(a,end=" ")
